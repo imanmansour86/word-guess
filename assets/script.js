d4658af4ab494c476b504b907329b022a8eb3f6f
Bradley Le <brabrele@gmail.com>
2022-01-03 14:48:34 -0800

    added pseudocode for the javascript

---
+++ assets/script.js
@@ -0,0 +1,31 @@
+// declare variables to reference DOM elements
+// word bank stored in array
+var words = ["array", "visual", "concatenate", "increment"];
+
+// function where user presses a button to start the game
+    // use setInterval() to set at 60 seconds and countdown at every 1 seconds (1,000 ms)
+    // have a function display the parts of the word with underscores as missing letters
+    // function with eventListeners that enable users to guess letters
+    // function to display outcome message and update scoreboard
+
+
+// have a function display the parts of the word with underscores as missing letters
+     // use random number generator to pick an element in the array as the word
+
+// function with eventListeners that enable users to guess letters
+    // reference eventListener to a variable
+    // use for loop to compare the eventListener to the element at the index of the array
+        // If true, display the letter
+        // Else, do nothing
+
+
+// function that uses localStorage to keep track of wins and losses
+    // use JSON.stringify to turn the word into a string for localStorage
+
+// function that reads localStorage to display on the web browser for the user to see
+    // use JSON.parse to turn the JSON string into actual usable information to display
+
+
+// function to display outcome message and update scoreboard
+    // If user wins, increment wins and display message
+    // Else statement when user losses, increment losses and display message
